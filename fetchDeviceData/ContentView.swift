7459526d0b6256d3b08f538e5ab898a2028c2665
Rumen Guin <rumen2k@gmail.com>
2023-04-23 15:17:58 +0530

    Initial Commit

---
+++ fetchDeviceData/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fetchDeviceData
+//
+//  Created by RUMEN GUIN on 23/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fetchDeviceData/fetchDeviceDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchDeviceDataApp.swift
+//  fetchDeviceData
+//
+//  Created by RUMEN GUIN on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct fetchDeviceDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
